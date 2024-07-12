86fb7ce728588cd95420cc23962ad5e8235fb9db
Cristian Camilo Garcia <cgarcia@caballo.com.co>
2024-07-12 15:52:50 -0500

    Se crearon peticiones CRUD para referencias

---
+++ src/controllers/reference.controller.js
@@ -0,0 +1,72 @@
+import { pool } from "../db.js";
+export const referenceController={
+    getReferences: async(req, res) => {
+        try {
+            const [rows] = await pool.query('SELECT * FROM listado_referencias');
+            res.send(rows);
+        } catch (error) {
+            console.error(error);
+        }
+    },
+    getReference:async(req, res) => {
+        const id = req.params.id;
+        try {
+            const [rows] = await pool.query('SELECT * FROM listado_referencias WHERE id_referencia = ?', [id]);
+            if (rows.length >0) {
+                res.send(rows[0]);
+            }else {
+            res.status(404).send('Maquina no encontrada');
+            }
+        } catch (error) {
+        console.error(error);
+        }
+    },
+    createreference:  async (req, res) => {
+        const { id_referencia,nombre_referencia, codigo_referencia} = req.body;
+        const [result] = await pool.query('INSERT INTO listado_referencias ( id_referencia,nombre_referencia, codigo_referencia) VALUES (?,?,?)', [ id_referencia,nombre_referencia, codigo_referencia])
+        res.send({ 
+            id: result.insertId,
+            nombre_referencia,
+            codigo_referencia,
+         });
+    },
+
+    updateReference: async (req, res) => {
+        const { id_referencia } = req.params; // Obtener id_usuarios desde los parámetros de la URL
+        const { codigo_referencia, nombre_referencia } = req.body;
+    
+        try {
+            const [result] = await pool.query(
+                'UPDATE listado_referencias SET codigo_referencia = ?, nombre_referencia = ? WHERE id_referencia = ?',
+                [codigo_referencia, nombre_referencia, id_referencia]
+            );
+    
+            if (result.affectedRows === 0) {
+                return res.status(404).json({ message: 'Usuario no encontrado' }); 
+            }
+    
+            const [rows] = await pool.query('SELECT * FROM listado_referencias WHERE id_referencia = ?', [id_referencia]);
+    
+            res.json(rows);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
+    deleteRereference: async(req, res) => {
+        const { id_referencia } = req.params;
+
+        try {
+            const [result] = await pool.query('DELETE FROM listado_referencias WHERE id_referencia = ?', [id_referencia]);
+    
+            if (result.affectedRows === 0) {
+                return res.status(404).json({ message: 'Usuario no encontrado' });
+            }
+    
+            res.json({ message: 'Usuario eliminado exitosamente' });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
+
+
+}
