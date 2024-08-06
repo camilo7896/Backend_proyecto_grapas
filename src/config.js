257fbbde477cdf51eaf0443778d109a7a826688d
Cristian Camilo Garcia <cgarcia@caballo.com.co>
2024-08-05 20:46:50 -0500

    Se reparo archivo config

--- src/config.js
+++ src/config.js
@@ -16,3 +16,14 @@ export const DB_DATABASE = process.env.DB_DATABASE || 'db_puntilas';
 export const DB_PORT = process.env.DB_PORT || 3306
 
 export const APP_URL = process.env.APP_URL || 'http://localhost';
+
+//mysql -hviaduct.proxy.rlwy.net -u gypcaballo -p nEQKrkCfshUjBcfWnAffdykWKAmXydvI --port 35902 --protocol=TCP railway
+/*
+DB_HOST=  viaduct.proxy.rlwy.net   mysql.railway.internal
+DB_USER=gypcaballo
+DB_PASSWORD=  SpxvpTfFTORMMfKaWrTTFHaARWWkzUqP
+DB_PORT=35902   3306
+DB_NAME=railway
+DB_PROTOCOL=TCP
+MYSQLUSER
+*/
